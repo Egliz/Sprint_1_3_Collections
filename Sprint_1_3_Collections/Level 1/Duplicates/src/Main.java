eb2fb52a2423fea7c1adf8729c346e6bb9d43b4c
Egliz <egliz3010@gmail.com>
2026-08-03 16:17:27 +0200

    feat: add addMonth method

--- Sprint_1_3_Collections/Level 1/Duplicates/src/Main.java
+++ Sprint_1_3_Collections/Level 1/Duplicates/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        //ArrayList<Month> listMonths = new ArrayList();
+
+
+    }
+
+    public static void addMonth(ArrayList<Month>listMonths) {
 
     }
 }
