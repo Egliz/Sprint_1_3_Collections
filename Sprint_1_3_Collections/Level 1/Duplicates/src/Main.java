426cbd168c93f81f62cb9f10d59faf16b3d31c09
Egliz <egliz3010@gmail.com>
2026-08-06 12:21:49 +0200

    feat: add checkDuplicateMonth

--- Sprint_1_3_Collections/Level 1/Duplicates/src/Main.java
+++ Sprint_1_3_Collections/Level 1/Duplicates/src/Main.java
@@ -1,27 +1,37 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
 
         ArrayList<Month> listMonths = new ArrayList();
 
-        System.out.println(addMonth(listMonths));
+        addMonth(listMonths);
+        System.out.println("Added months: " + listMonths);
 
         Month agost = new Month("Agost");
         listMonths.add(7, agost);
 
-        System.out.println("The missing month was added to its " +
-                "corresponding position, and the list remains in order: " + listMonths);
+        System.out.println("\nThe missing month was added to its " +
+                "corresponding position, and the list remains in order:\n" + listMonths);
 
         HashSet<Month> hSet = new HashSet<>(listMonths);
-        System.out.println(hSet);
+        System.out.println(checkDuplicateMonth(hSet));
 
+        System.out.println("***** Displaying the months using a 'for loop':... *****\n");
+        for(Month month : hSet) {
+            System.out.println(month);
+        }
 
-
+        System.out.println("***** Displaying the months with 'Iterator':... *****");
+        Iterator<Month> it = hSet.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 
-    public static String addMonth(ArrayList<Month>listMonths) {
+    public static void addMonth(ArrayList<Month>listMonths) {
 
         listMonths.add(new Month ("Gener"));
         listMonths.add(new Month ("Febrer"));
@@ -34,7 +44,11 @@ public class Main {
         listMonths.add(new Month ("Octubre"));
         listMonths.add(new Month ("Novembre"));
         listMonths.add(new Month ("Decembre"));
+    }
 
-        return "Added months: " + listMonths;
+    public static String checkDuplicateMonth(HashSet<Month>hSet) {
+        Month duplMonth = new Month("Febrer");
+        hSet.add(duplMonth);
+        return "Months without duplicates:\n" + hSet;
     }
 }
