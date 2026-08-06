155565fa86301bb84d069babcc5ecdb85c225435
Egliz <egliz3010@gmail.com>
2026-08-06 20:46:09 +0200

    feat: add second list integer

--- Sprint_1_3_Collections/src/level_1/ListIterator/Main.java
+++ Sprint_1_3_Collections/src/level_1/ListIterator/Main.java
@@ -1,11 +1,25 @@
 package level_1.ListIterator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    int sizeList = 6;
+    public static void main(String[] args) {
+        int numsInList = 6;
 
-    public static List<Integer> firstList = new List<Integer>();
+        List<Integer> firstList = new ArrayList<Integer>();
+        for(int i = 0; i < numsInList; i++){
+            firstList.add(i);
+        }
+        System.out.println(firstList);
 
+        List<Integer> scondList = new ArrayList<Integer>();
+        for (int i = numsInList - 1; i >= 0; i--) {
+            scondList.add(i);
+        }
+        System.out.println(scondList);
+
+
+    }
 
 }
