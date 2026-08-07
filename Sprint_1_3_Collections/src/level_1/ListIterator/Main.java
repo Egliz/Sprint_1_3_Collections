935eaa19d8d9da833ee3ea681479510745cee172
Egliz <egliz3010@gmail.com>
2026-08-07 13:08:23 +0200

    refactor: add list reversal to a method

--- Sprint_1_3_Collections/src/level_1/ListIterator/Main.java
+++ Sprint_1_3_Collections/src/level_1/ListIterator/Main.java
@@ -7,25 +7,29 @@ import java.util.ListIterator;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("First List:\n" + OriginalList());
+        System.out.println("First List:\n" + originalList());
+        System.out.println("Second List:\n" + reverseList(originalList()));
 
-       /*
-        List<Integer> secondList = new ArrayList<Integer>();
-        ListIterator<Integer> it = firstList.listIterator(firstList.size());
-
-        while (it.hasPrevious()) {
-            secondList.add(it.previous());
-        }
-        System.out.println("Second List:\n" + secondList);
     }
-*/
-}
-    public static List<Integer> OriginalList(){
+
+    public static List<Integer> originalList() {
         List<Integer> firstList = new ArrayList<Integer>();
         int numsInList = 6;
         for (int i = 0; i < numsInList; i++) {
             firstList.add(i);
         }
         return firstList;
+    }
+
+    public static List<Integer> reverseList(List<Integer> originalList) {
+
+        List<Integer> secondList = new ArrayList<Integer>();
+        ListIterator<Integer> it = originalList.listIterator(originalList.size());
+
+        while (it.hasPrevious()) {
+            secondList.add(it.previous());
+        }
+        return secondList;
+
     }
 }
