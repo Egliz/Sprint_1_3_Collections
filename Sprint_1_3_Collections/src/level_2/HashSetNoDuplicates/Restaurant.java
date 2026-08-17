a532bdd84196a479192fb1f7b93ee1bfa0cdd868
Egliz <egliz3010@gmail.com>
2026-08-17 13:53:46 +0200

    feat: add Restaurant class

--- Sprint_1_3_Collections/src/level_2/HashSetNoDuplicates/Restaurant.java
+++ Sprint_1_3_Collections/src/level_2/HashSetNoDuplicates/Restaurant.java
@@ -1,3 +1,13 @@
 package level_2.HashSetNoDuplicates;
 
+public class Restaurant {
+
+    private String name;
+    private int score;
+
+    public Restaurant (String name, int score){
+        this.name = name;
+        this.score = score;
+
+    }
 }
