34081846364860bf3e0a87f8cf8e36b523abeb80
Egliz <egliz3010@gmail.com>
2026-08-18 20:53:06 +0200

    feat: add Main class

---
+++ Sprint_1_3_Collections/src/level_2/MultipleSorting/Main.java
@@ -0,0 +1,7 @@
+package level_2.MultipleSorting;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- Sprint_1_3_Collections/src/level_2/MultipleSorting/RestaurantComparator.java
+++ Sprint_1_3_Collections/src/level_2/MultipleSorting/RestaurantComparator.java
@@ -4,4 +4,21 @@ import java.util.Comparator;
 
 public class RestaurantComparator implements Comparator<Restaurant> {
 
+    @Override
+    public static int compare(Restaurant o1, Restaurant o2) {
+        int result = o1.getName().compareTo(o2.getName());
+
+        if (result < 0) {
+            return -1;
+        } else if (result > 0) {
+            return 1;
+        } else {
+            if (o1.getScore() > o2.getScore()) {
+                return -1;
+            } else if (o1.getScore() < o2.getScore()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
 }
